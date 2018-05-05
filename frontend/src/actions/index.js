eab835ec74e05c0d8262bca75b6a7aa51dfc863a
Subhi Dweik <subhime@gmail.com>
2018-05-05 19:39:30 +0200

    Removed counter

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,18 +1,16 @@
 import { location } from '@hyperapp/router'; // eslint-disable-line
 import auth from './auth';
 import commandresults from './commandresults';
-import counter from './counter';
 import profile from './profile';
 import repositories from './repositories';
 import users from './users';
 
 export default {
    auth,
-   counter,
+   commandresults,
+   location: location.actions,
+   profile,
    repositories,
    users,
-   commandresults,
-   profile,
-   location: location.actions,
 };
 
--- frontend/src/state/index.js
+++ frontend/src/state/index.js
@@ -1,17 +1,15 @@
 import { location } from '@hyperapp/router'; // eslint-disable-line
 import auth from './auth';
 import commandresults from './commandresults';
-import counter from './counter';
 import profile from './profile';
 import repositories from './repositories';
 import users from './users';
 
 export default {
    auth,
-   counter,
+   commandresults,
+   location: location.state,
    profile,
    repositories,
-   commandresults,
    users,
-   location: location.state,
 };
